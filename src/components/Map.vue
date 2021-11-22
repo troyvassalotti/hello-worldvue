5636a306c5fd362850392b7f16c6d79cce9ddcf0
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-11-22 13:39:29 -0500

    Allow basic support for GPX files

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,7 +1,12 @@
 <script>
+/*
+TODO
+- Better handling of multiple files, i.e. removing old layers or mappings when a new file is added.
+ */
 import "leaflet/dist/leaflet.css"
 import L from "leaflet"
 import "leaflet-kmz/dist/leaflet-kmz"
+import "leaflet-gpx/gpx"
 import {mapboxAccessKey} from "../mapbox";
 
 export default {
@@ -14,6 +19,7 @@ export default {
     }
   },
   methods: {
+    // Create the initial Leaflet map
     setupLeafletMap: function () {
       this.mapDiv = L.map('map', {preferCanvas: true})
           .setView(this.center, 2)
@@ -26,20 +32,36 @@ export default {
       }).addTo(this.mapDiv)
     },
     parseFile: function (el) {
+      // Check to see if there are any Object URLs to revoke
       if (this.currentObjectURLs.length > 0) {
         for (let obj = 0; obj < this.currentObjectURLs.length; obj++) {
           URL.revokeObjectURL(this.currentObjectURLs[obj])
         }
+        this.currentObjectURLs = [] // reset the objectURLs array since we are only handling one file at the moment
       }
+
+      // Set some variables for use later
       const fileList = el.files
-      const objectURL = URL.createObjectURL(fileList[0])
+      const firstFile = fileList[0]
+      const objectURL = URL.createObjectURL(firstFile)
+
       this.currentObjectURLs.push(objectURL)
-      let kmz = L.kmzLayer().addTo(this.mapDiv)
-      let control = L.control.layers(null, null, {collapsed: false}).addTo(this.mapDiv)
-      kmz.on('load', e => {
-        control.addOverlay(e.layer, e.name)
-      })
-      kmz.load(objectURL)
+
+      if (firstFile.type === "application/gpx+xml") {
+        // Parse GPX files
+        let gpx = objectURL
+        new L.GPX(gpx, {async: true}).on('loaded', e => {
+          this.mapDiv.fitBounds(e.target.getBounds())
+        }).addTo(this.mapDiv)
+      } else {
+        // Parse KML and KMZ files
+        let kmz = L.kmzLayer().addTo(this.mapDiv)
+        let control = L.control.layers(null, null, {collapsed: false}).addTo(this.mapDiv)
+        kmz.on('load', e => {
+          control.addOverlay(e.layer, e.name)
+        })
+        kmz.load(objectURL)
+      }
     }
   },
   mounted() {
@@ -51,7 +73,7 @@ export default {
 <template>
   <main>
     <form enctype="multipart/form-data" novalidate>
-      <label for="upload">Upload Your Map <input id="upload" type="file" accept=".kmz, .kml" @change="parseFile($event.target)"></label>
+      <label for="upload">Upload Your Map <input id="upload" type="file" accept=".kmz, .kml, .gpx" @change="parseFile($event.target)"></label>
     </form>
     <div id="map"></div>
   </main>
