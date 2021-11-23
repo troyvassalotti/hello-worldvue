7531fab0c5d4666f456fb14da902de094be2459b
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-11-23 09:23:23 -0500

    add privacy statement

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import "@exampledev/new.css/new.css"
 import HelloWorld from './components/HelloWorld.vue'
 import Map from './components/Map.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
   <HelloWorld msg="Hello, WorldVue!"/>
   <Map/>
+  <Footer msg="This is private."/>
 </template>
