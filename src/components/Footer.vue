c9e6d29b5f4e5d04389de78036ff0955f59a15a8
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2024-05-25 23:29:16 -0400

    Update some packages

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,7 +10,3 @@ defineProps({
     <p>This app doesn't use a database and I have no way of knowing details about the files you upload. I've restricted you to only uploading specific file types and that's that. All files are accessed in the browser and loaded as Object URLs in the map. When you close this window, those Object URLs are revoked (as is the nature of Object URLs) and you can safely reload this app at square one.</p>
   </footer>
 </template>
-
-<style scoped>
-
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,17 +7,7 @@ defineProps({
 <template>
   <header>
     <h1>{{ msg }}</h1>
-    <p class="subtitle">hahaha, get it?</p>
+    <p><em>hahaha, get it?</em></p>
     <p>Hello WorldVue is a <a href="https://v3.vuejs.org/">Vue 3</a> and <a href="https://vitejs.dev/">Vite</a> app for visualizing KMZ, KML, and GPX files in a <a href="https://leafletjs.com">Leaflet</a> map. To view your map, upload a relevant track below.</p>
   </header>
 </template>
-
-<style scoped>
-header {
-  text-align: center;
-}
-
-.subtitle {
-  font-style: italic;
-}
-</style>
